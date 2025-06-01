7f53fd6d89a9d774a90f449a6d3f1b678a810ba6
sachin <jithurisachin@gmail.com>
2025-06-01 20:14:30 +0530

    updated code

--- todoproject/src/TodoList.jsx
+++ todoproject/src/TodoList.jsx
@@ -1,65 +1,188 @@
-import { useState } from 'react'
-import './todo.css'
+import { useEffect, useState } from "react";
+import "./todo.css";
 
+const statusList = [
+  { label: "All", value: "all" },
+  { label: "Completed", value: "completed" },
+  { label: "Not Completed", value: "not-completed" },
+];
 const TodoList = () => {
- const [value, setValue] = useState('')
-  const [items, setItems] = useState([])
-  const [isEdit, setIsEdit] = useState(null)
+  const [inputValue, setInputValue] = useState({
+    value: "",
+    checked: false,
+    id: Math.random(),
+  });
+  const [items, setItems] = useState([]);
+  const [isEdit, setIsEdit] = useState(null);
+  const [status, setStatus] = useState("all");
+  const [filteredItems, setFilteredItems] = useState([]);
 
   const handleChange = (event) => {
-    setValue(event.target.value)
-  }
+    setInputValue((prev) => ({ ...prev, value: event.target.value }));
+  };
 
   const handleClick = () => {
-    if(isEdit){
-        setItems(prev => prev.map((e,i) => i+1 === isEdit ? value : e))
-    }else{
-        setItems(prev => ([...prev, value]))
+    if (inputValue.value) {
+      if (isEdit) {
+        setItems((prev) => prev.map((e) => (e.id === isEdit ? inputValue : e)));
+      } else {
+        setItems((prev) => [...prev, inputValue]);
+      }
+      setInputValue({ value: "", checked: false, id: Math.random() });
+      setIsEdit(null);
     }
-    setValue('');
-    setIsEdit(null)
-  }
+  };
 
-  const handleDelete = (idx) => {
-    setItems(prev => prev.map((e,i) => i === idx ? null : e).filter(e => e))
-  }
+  const handleDelete = (id) => {
+    setItems((prev) =>
+      prev.map((e) => (e.id === id ? null : e)).filter((e) => e)
+    );
+  };
 
+  const handleEdit = (id) => {
+    setIsEdit(id);
+    setInputValue(items.find((e) => e.id === id));
+  };
 
-  const handleEdit = (i) => {
-    setIsEdit(i+1)
-    setValue(items[i])
-  }
+  const handleChangeCheckbox = (id) => {
+    setItems((prev) =>
+      prev.map((item) =>
+        item.id === id ? { ...item, checked: !item.checked } : item
+      )
+    );
+  };
+
+  const handleChangeStatus = (status) => {
+    setStatus(status);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleClick();
+    }
+  };
+
+  const handleReset = () => {
+    setItems([]);
+  };
+
+  useEffect(() => {
+    switch (status) {
+      case "all":
+        setFilteredItems(items);
+        break;
+      case "completed":
+        setFilteredItems(items.filter((e) => e.checked));
+        break;
+      case "not-completed":
+        setFilteredItems(items.filter((e) => !e.checked));
+        break;
+      default:
+        setFilteredItems(items);
+        break;
+    }
+  }, [status, setFilteredItems, items]);
 
   return (
     <div className="container">
-        <div>
-                <h1 className="todo_title">TODO APP</h1>
-                <div className="input_container">
-                 <input className="todo_input" type="text" placeholder="Enter Input" value={value} onChange={handleChange}/>
-                 <button className="todo_button" onClick={handleClick}>{isEdit ? "Update" :"Add Todo"}  </button>
-                </div>
-
-                <div>
-                    <ol className="todo_list_container">
-                        {
-                            items.filter(e => e).map((each,i) => {
-                                return(
-                                    <li key={each} className="todo_list_item">
-                                        <div>{each}</div>
-                                        <div className="todo_list_item_actions">
-                                            <div><button onClick={() => handleEdit(i)}>Edit</button></div>
-                                            <div><button onClick={() => handleDelete(i)}>Delete</button></div>
-                                        </div>
-                                    </li>
-                                )
-                            })
-                        }
-
-                    </ol>
-                </div>
+      <div>
+        <h1 className="todo_title">TODO APP</h1>
+        <div className="input_container">
+          <input
+            className="todo_input"
+            type="text"
+            placeholder="Enter Input"
+            value={inputValue.value}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+          />
+          <button className="todo_button" onClick={handleClick}>
+            {isEdit ? "Update" : "Add Todo"}{" "}
+          </button>
         </div>
-    </div>
-  )
-}
 
-export default TodoList
+        <div className="input_container" style={{ marginTop: "5px" }}>
+          {items.length > 0 && statusList.map((e) => (
+            <button
+              style={{
+                backgroundColor: e.value === status ? "black" : "",
+                color: e.value === status ? "white" : "",
+                fontSize: 20,
+                marginRight: "10px",
+              }}
+              key={e.value}
+              onClick={() => handleChangeStatus(e.value)}
+            >
+              {e.label}
+            </button>
+          ))}
+        </div>
+
+        <div>
+          <ol className="todo_list_container">
+            {filteredItems
+              .filter((e) => e)
+              .map((each) => {
+                return (
+                  <li key={each.id} className="todo_list_item">
+                    <div>
+                      <input
+                        type="checkbox"
+                        onChange={() => handleChangeCheckbox(each.id)}
+                        checked={each.checked}
+                      />{" "}
+                    </div>
+                    <div
+                      style={
+                        each.checked
+                          ? { textDecorationLine: "line-through" }
+                          : {}
+                      }
+                    >
+                      {each.value}
+                    </div>
+                    <div className="todo_list_item_actions">
+                      <div>
+                        <button onClick={() => handleEdit(each.id)}>
+                          Edit
+                        </button>
+                      </div>
+                      <div>
+                        <button onClick={() => handleDelete(each.id)}>
+                          Delete
+                        </button>
+                      </div>
+                    </div>
+                  </li>
+                );
+              })}
+          </ol>
+        </div>
+        {
+            filteredItems.length > 0 ?
+                <div
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+                >
+                <button
+                    style={{
+                    backgroundColor: "black",
+                    color: "white",
+                    fontSize: 20,
+                    }}
+                    onClick={handleReset}
+                >
+                    Reset all
+                </button>
+                </div>
+            :null
+        }
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
